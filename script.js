1f3fbba6b7a04a616bdb023c4294e2488df22848
anmolmehra1717 <anmolmehra053@gmail.com>
2022-09-06 17:25:39 +0530

    added darkmode button

---
+++ script.js
@@ -0,0 +1,15 @@
+const options ={
+    bottom: '36rem', 
+  right: '15px', 
+  left: 'unset', 
+  time: '0.5s',
+  mixColor: '#fff', 
+  backgroundColor: '#fff',  
+  buttonColorDark: '#100f2c',  
+  buttonColorLight: '#fff', 
+  saveInCookies: false, 
+  label: '🌓', 
+  autoMatchOsTheme: true 
+}
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
